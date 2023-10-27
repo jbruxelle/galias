2ba8f73a24aa7151336df83888f20cd087d739cf
Julian BRUXELLE <36154424+jbruxelle@users.noreply.github.com>
2023-10-27 13:11:08 +0200

    :fire: - IMPROVE: cleaned vite config

--- galias/vite.config.ts
+++ galias/vite.config.ts
@@ -1,34 +1,8 @@
-import { resolve } from "path";
 import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
 
 export default defineConfig({
   test: {
     globals: true,
     exclude: ["samples", "node_modules"],
   },
-  plugins: [dts({ rollupTypes: true })],
-  // build: {
-  //   lib: {
-  //     entry: {
-  //       index: resolve(__dirname, "./adapters/primary/vite-plugin/plugin.ts"),
-  //       adapters: resolve(
-  //         __dirname,
-  //         "./adapters/secondary/gateways/configuration"
-  //       ),
-  //     },
-  //     name: "galias",
-  //     formats: ["es", "cjs"],
-  //   },
-  //   rollupOptions: {
-  //     external: ["fs/promises", "path", "path/posix", "glob", "picomatch"],
-  //     input: {
-  //       index: resolve(__dirname, "./adapters/primary/vite-plugin/plugin.ts"),
-  //       adapters: resolve(
-  //         __dirname,
-  //         "./adapters/secondary/gateways/configuration"
-  //       ),
-  //     },
-  //   },
-  // },
 });
